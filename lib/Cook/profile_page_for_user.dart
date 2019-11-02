de3a51d38ee3e6b6e60936addaef84dfac132bf7
VISHNU JANGID <vishnu.jangid@boxnbiz.com>
2019-11-02 20:31:17 +0530

    First commit

--- lib/Cook/profile_page_for_user.dart
+++ lib/Cook/profile_page_for_user.dart
@@ -1,15 +1,15 @@
 import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:find_my_cook_app/loader/loader.dart';
 
 String current_cook_image =
     'https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg';
 String cook_name = 'vishnu Jangid';
-String cook_native = 'chomu,jaipur,Rajasthan';
-int food_prepared_type = 0; //0(veg),1(non-veg),2(both)
-double rating = 1.5;
+String cook_native = 'chomu, jaipur, Rajasthan';
+int food_prepared_type = 2; //0(veg),1(non-veg),2(both)
+double coock_rating = 3.5;
 double experince = 2;
-int numbe_of_person_rating_given = 2;
+int no_person_rating_given = 121;
 
 class ProfilePageForUser extends StatefulWidget {
   @override
@@ -17,24 +17,72 @@ class ProfilePageForUser extends StatefulWidget {
 }
 
 class _ProfilePageForUserState extends State<ProfilePageForUser> {
+  double rating = 3.5;
+
+  @override
+  Container get_food_color_dot(var v) {
+    if (food_prepared_type == 0) {
+      return new Container(
+        child: new Row(
+          children: <Widget>[
+            new Text("food prepare  "),
+            new Dot(
+              radius: 12.0,
+              color: Colors.green,
+            )
+          ],
+        ),
+      );
+    } else if (food_prepared_type == 1) {
+      return new Container(
+        child: new Row(
+          children: <Widget>[
+            new Text("food prepare "),
+            new Dot(
+              radius: 12.0,
+              color: Colors.red,
+            )
+          ],
+        ),
+      );
+    } else {
+      return new Container(
+        child: new Row(
+          children: <Widget>[
+            new Text("food prepare "),
+            new Dot(
+              radius: 10.0,
+              color: Colors.green,
+            ),
+            new Text("  "),
+            new Dot(
+              radius: 10.0,
+              color: Colors.red,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Stack(
         children: <Widget>[
-          ClipPath(
+          new ClipPath(
             child: Container(
               color: Colors.black.withOpacity(0.8),
             ),
             clipper: getClipper(),
           ),
-          Positioned(
+          new Positioned(
             width: 350.0,
             left: 25.0,
             top: MediaQuery.of(context).size.height / 5,
             child: Column(
               children: <Widget>[
-                Container(
+                new Container(
                   width: 150.0,
                   height: 150.0,
                   decoration: BoxDecoration(
@@ -47,7 +95,7 @@ class _ProfilePageForUserState extends State<ProfilePageForUser> {
                         BoxShadow(blurRadius: 7.0, color: Colors.black)
                       ]),
                 ),
-                SizedBox(height: 90.0),
+                SizedBox(height: 70.0),
                 Text(
                   cook_name,
                   style: TextStyle(
@@ -63,8 +111,57 @@ class _ProfilePageForUserState extends State<ProfilePageForUser> {
                       fontStyle: FontStyle.italic,
                       fontFamily: 'Monteserrat'),
                 ),
-                SizedBox(height: 25.0),
-                Container(
+                SizedBox(height: 5.0),
+                new Container(
+                  height: 30.0,
+                  width: 120.0,
+                  child: get_food_color_dot(food_prepared_type),
+                ),
+                new Container(
+                  height: 30.0,
+                  width: 120.0,
+                  child: new Center(
+                    child: new Text(
+                      (experince.toString() + " Year"),
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 17.0,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                new Center(
+                  child: new Container(
+                    height: 30.0,
+                    width: 120.0,
+                    child: new StarRating(
+                      rating: coock_rating,
+                      onRatingChanged: (rating) =>
+                          setState(() => this.rating = rating),
+                    ),
+                  ),
+                ),
+                new Container(
+                  height: 40.0,
+                  width: 120.0,
+                  child: new Center(
+                    child: new Row(
+                      children: <Widget>[
+                        new IconButton(
+                          icon: Icon(Icons.mood),
+                          color: Colors.green,
+                        ),
+                        new Text(no_person_rating_given.toString(),
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                fontSize: 17.0)),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                new Container(
                   height: 30.0,
                   width: 120.0,
                   child: Material(
@@ -85,7 +182,30 @@ class _ProfilePageForUserState extends State<ProfilePageForUser> {
                       ),
                     ),
                   ),
-                )
+                ),
+                SizedBox(height: 10.0),
+                new Container(
+                  height: 30.0,
+                  width: 120.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    shadowColor: Colors.blueAccent,
+                    color: Colors.blue,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: () {
+                        //NEED TO UPDATE---------------------------------------------------
+                      },
+                      child: Center(
+                        child: Text(
+                          'check profile',
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Monteserrat'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
@@ -112,11 +232,46 @@ class getClipper extends CustomClipper<Path> {
   }
 }
 
-class Dot extends StatelessWidget {
+typedef void RatingChangeCallback(double rating);
+
+class StarRating extends StatelessWidget {
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color color;
+
+  StarRating(
+      {this.starCount = 5, this.rating = .0, this.onRatingChanged, this.color});
+
+  Widget buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = new Icon(
+        Icons.star_border,
+        color: Theme.of(context).buttonColor,
+      );
+    } else if (index > rating - 1 && index < rating) {
+      icon = new Icon(
+        Icons.star_half,
+        color: color ?? Theme.of(context).primaryColor,
+      );
+    } else {
+      icon = new Icon(
+        Icons.star,
+        color: color ?? Theme.of(context).primaryColor,
+      );
+    }
+    return new InkResponse(
+      onTap:
+          onRatingChanged == null ? null : () => onRatingChanged(index + 1.0),
+      child: icon,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-    );
+    return new Row(
+        children:
+            new List.generate(starCount, (index) => buildStar(context, index)));
   }
 }
